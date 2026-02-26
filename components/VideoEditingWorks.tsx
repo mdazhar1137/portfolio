62a647b3ffd3ef93096c77ccbec90aec6b344dc1
mdazhar1137 <mdazhar1137@gmail.com>
2026-02-26 21:46:31 +0530

    ch

--- components/VideoEditingWorks.tsx
+++ components/VideoEditingWorks.tsx
@@ -71,10 +71,8 @@ export default function VideoEditingWorks() {
           </p>
         </motion.div>
 
-        {/* Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
-                gap-6 lg:gap-8 
-                px-5 lg:px-6">
+    {/* Grid */}
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 px-5 lg:px-6">
           {videos.map((v, i) => (
             <motion.div
               key={i}
